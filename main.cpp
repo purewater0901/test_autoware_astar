fd402a7ce4d6682156460b88c27ca44629df18e3
yutaka <purewater0901@gmail.com>
2020-12-06 15:22:37 -0800

    add velocity filter

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@
 
 int main() {
 
+    const int N = 55;
+    const double initial_vel = 7.0;
+    const double initial_acc = -0.8;
+    const double ds = 1.0;
+    const double jerk_acc = 0.8;
+    const double max_accel = 1.0;
+
     AStarOptimizer::AStarOptimizerParam astar_param{};
     astar_param.max_accel = 1.0;
     astar_param.min_decel = -1.0;
@@ -16,72 +23,70 @@ int main() {
     astar_param.weight_jerk = 1.0;
     astar_param.weight_over_v = 1e6;
     astar_param.dt = 0.1;
-    astar_param.ds = 1.0;
-    astar_param.dv = 0.1;
+    astar_param.ds = ds;
+    astar_param.dv = 0.3;
     astar_param.max_time = 30.0;
-
-    int N = 55;
-    //double initial_velocity = 8.13652;
-    double initial_velocity = 7.0;
-    double initial_acceleration = -0.8;
 
     std::vector<double> s_longitudinal(N, 0.0);
     std::vector<double> v_longitudinal(N, 0.0);
 
     // position
     for(int i=0; i<N; ++i)
-        s_longitudinal[i] = i*1.0;
+        s_longitudinal[i] = i*ds;
 
-    for(int i=0; i<N; ++i)
+    for(int i=0; i<30; ++i)
         v_longitudinal[i] = 8.0;
-    //v_longitudinal.back() = 0.0;
+    for(int i=30; i<N; ++i)
+        v_longitudinal[i] = 10.0;
+    v_longitudinal.back() = 0.0;
 
-    /*
-    v_longitudinal[0] = 11.661;
-    v_longitudinal[1] = 9.37006;
-    v_longitudinal[2] = 8.21379;
-    v_longitudinal[3] = 8.18014;
-    v_longitudinal[4] = 7.66211;
-    v_longitudinal[5] = 5.67608;
-    v_longitudinal[6] = 3.85434;
-    v_longitudinal[7] = 3.0538;
-    v_longitudinal[8] = 2.76943;
-    for(int i=9; i<=26; ++i)
-        v_longitudinal[i] = 2.74;
-    v_longitudinal[27] = 2.81086;
-    v_longitudinal[28] = 3.00792;
-    v_longitudinal[29] = 3.28135;
-    v_longitudinal[30] = 3.63436;
-    v_longitudinal[31] = 4.14298;
-    v_longitudinal[32] = 4.98283;
-    v_longitudinal[33] = 6.68678;
-    v_longitudinal[34] = 8.53884;
-    v_longitudinal[35] = 8.53884;
-    v_longitudinal[36] = 8.94713;
-    v_longitudinal[37] = 12.3301;
-    for(int i=38; i<=41; ++i)
-        v_longitudinal[i] = 13.8889;
-    v_longitudinal[42] = 13.8845;
-    v_longitudinal[43] = 13.6195;
-    v_longitudinal[44] = 12.6215;
-    v_longitudinal[45] = 11.7284;
-    v_longitudinal[46] = 11.0689;
-    v_longitudinal[47] = 10.8736;
-    for(int i=48; i<=51; ++i)
-        v_longitudinal[i] = 10.8695;
-    v_longitudinal[52] = 10.92;
-    v_longitudinal[53] = 11.2283;
-    v_longitudinal[54] = 0.0;
-     */
 
-    double s_goal = 53.8351;
+    // 2. Forward Filter
+    std::vector<double> filtered_velocity(v_longitudinal.size());
+    filtered_velocity.front() = initial_vel;
+    double current_vel = initial_vel;
+    double current_acc = initial_acc;
+    for(int i=1; i<N; ++i)
+    {
+        double dt = 0.0;
+        if(std::fabs(current_vel)<1e-6)
+            dt = sqrt(2*ds/max_accel);
+        else
+            dt = ds/current_vel;
+
+        current_acc = std::max(current_acc + jerk_acc*dt, max_accel);
+        current_vel = std::min(current_vel + current_acc * dt, v_longitudinal[i]);
+
+        // Store Filtered Velocity
+        filtered_velocity[i] = current_vel;
+    }
+
+    //3. Backward Filter
+    filtered_velocity.back() = v_longitudinal.back();
+    current_vel = v_longitudinal.back();
+    current_acc = 0.0;
+    for(int i=N-2; i>=0; --i)
+    {
+        double dt;
+        if(std::fabs(current_vel)<1e-4)
+            dt = sqrt(2*ds/max_accel);
+        else
+            dt = ds/current_vel;
+
+        current_acc = std::max(current_acc + jerk_acc*dt, max_accel);
+        current_vel = std::min(current_vel + current_acc * dt, filtered_velocity[i]);
+
+        // Store Filtered Velocity
+        filtered_velocity[i] = current_vel;
+    }
 
     for(int i=0; i<v_longitudinal.size(); ++i)
-        std::cout << "v[" << i << "]: " << v_longitudinal[i] << std::endl;
+        std::cout << "v[" << i << "]: " << v_longitudinal[i] << " Filtered Velocity: " << filtered_velocity[i] << std::endl;
+
 
     AStarOptimizer astar_optimizer(astar_param);
     AStarOptimizer::AStarOutputInfo astar_output_info;
-    astar_optimizer.solve(initial_velocity, initial_acceleration, 0, v_longitudinal, s_longitudinal,
+    astar_optimizer.solve(initial_vel, initial_acc, 0, filtered_velocity, s_longitudinal,
                             astar_output_info);
 
     /*
